0bbcc96519fb6c100aa9f4db6088b94fe57f5d9a
tonr-Cao <ctongrui@outlook.com>
2020-09-21 16:24:01 +0800

    提交文件

---
+++ src/main/java/com/tonr/git/HelloGit.java
@@ -0,0 +1,18 @@
+package com.tonr.git;
+
+/**
+ * @version: V1.0
+ * @author: tonr
+ * @className: HelloGit
+ * @packageName: com.tonr.git
+ * @data: 2020-09-21 4:01 下午
+ **/
+public class HelloGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("helloGit");
+
+    }
+
+}
